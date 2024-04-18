049c91df5f128741e72d7e6fd48cf91954085384
Wade Glaser <wade@innovateedunyc.org>
2024-04-18 11:05:48 -0400

    edit prefect yaml

--- wg_seatgeek/wg_seatgeek.py
+++ wg_seatgeek/wg_seatgeek.py
@@ -1,8 +1,10 @@
 """
 Prefect flow for use with seatgeek-client
 """
-from anyio import run
+# from anyio import run
 from prefect import flow, get_run_logger
+from prefect.blocks.system import Secret
+from prefect.deployments import DeploymentImage
 
 
 @flow
@@ -17,4 +19,16 @@ async def wg_seatgeek(team: str) -> None:
 
 
 if __name__ == "__main__":
-    run(wg_seatgeek, "Boston-Celtics")
+    artifact_reg_url: Secret = Secret.load("artifact-reg-url")
+
+    wg_seatgeek.deploy(
+        name="my-code-baked-into-an-image-deployment",
+        work_pool_name="central-push-pool",
+        image=DeploymentImage(
+            name="us-central1-docker.pkg.dev/wade-prefect/prefect-images/prefect-flows/wg-seatgeek",  # noqa
+            tag="test-tag",
+            dockerfile="Dockerfile",
+            platform="linux/amd64",
+            buildargs={"AUTHED_ARTIFACT_REG_URL": artifact_reg_url.get()},
+        ),
+    )
