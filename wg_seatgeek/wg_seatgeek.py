6761dc152dd41c9742abcdd771b257670fbe118a
Wade Glaser <wade@innovateedunyc.org>
2024-04-19 09:50:00 -0400

    change hello world message for testing

--- wg_seatgeek/wg_seatgeek.py
+++ wg_seatgeek/wg_seatgeek.py
@@ -4,7 +4,6 @@ Prefect flow for use with seatgeek-client
 # from anyio import run
 from prefect import flow, get_run_logger
 from prefect.blocks.system import Secret
-from prefect.deployments import DeploymentImage
 
 
 @flow
@@ -15,20 +14,9 @@ async def wg_seatgeek(team: str) -> None:
     """
 
     logger = get_run_logger()
-    logger.info("Hello World")
+    logger.info("Hello World Test 2")
 
 
 if __name__ == "__main__":
     artifact_reg_url: Secret = Secret.load("artifact-reg-url")
-
-    wg_seatgeek.deploy(
-        name="my-code-baked-into-an-image-deployment",
-        work_pool_name="central-push-pool",
-        image=DeploymentImage(
-            name="us-central1-docker.pkg.dev/wade-prefect/prefect-images/prefect-flows/wg-seatgeek",  # noqa
-            tag="test-tag",
-            dockerfile="Dockerfile",
-            platform="linux/amd64",
-            buildargs={"AUTHED_ARTIFACT_REG_URL": artifact_reg_url.get()},
-        ),
-    )
+    # do nothing
