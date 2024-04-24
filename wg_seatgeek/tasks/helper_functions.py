d7598e70349c85c512a49d50dcd68205661831ce
Wade Glaser <wade@innovateedunyc.org>
2024-04-24 11:12:03 -0400

    add helper functin to remove blank dicts in results

---
+++ wg_seatgeek/tasks/helper_functions.py
@@ -0,0 +1,24 @@
+"""_summary_
+"""
+
+
+def replace_blank_dict(d):
+    """_summary_
+
+    Args:
+        d:
+
+    Returns:
+        _type_:
+    """
+    if not d:
+        return None
+    if type(d) is list:
+        for list_item in d:
+            if type(list_item) is dict:
+                for k, v in list_item.items():
+                    list_item[k] = replace_blank_dict(v)
+    if type(d) is dict:
+        for k, v in d.items():
+            d[k] = replace_blank_dict(v)
+    return d
